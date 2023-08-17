22f8d7cf0a3ee730aac91553a11a9e64c3ae42df
Calin <acalin729@gmail.com>
2023-08-17 23:12:24 +0300

    displaying quote after getting data doesn't work

--- quotes-game-react/src/App.js
+++ quotes-game-react/src/App.js
@@ -2,16 +2,34 @@ import { Route, Routes } from 'react-router';
 import Home from './Home/Home';
 import Game from './Game/Game';
 import './App.css';
+import { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path='/' Component={Home} />
-        <Route path='game' element={<Game />} />
-      </Routes> 
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    data: []
+  }
+
+  componentDidMount() {
+      axios.get('https://api.api-ninjas.com/v1/quotes?category=',{
+          headers: { 'X-Api-Key': 't5uH4CHnIvhCe1J1jemWVA==oKu96FgPeJT5o3np'}
+      }).then(resp => {
+          console.log(resp);
+          this.setState({data: resp.data});
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Routes>
+          <Route path='/' Component={Home} />
+          <Route path='game' element={<Game quote={this.state.data.quote} />} />
+        </Routes> 
+      </div>
+    );
+  }
 }
 
 export default App;
--- quotes-game-react/src/Game/Game.js
+++ quotes-game-react/src/Game/Game.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from 'axios';
 
-const Game = () => {
+const Game = (props) => {
+
+
+
     return(
         <div className="components">
-            Game!
+            <h2>Who said that quote?</h2>
+            {
+                props.quote ? <div>{props.quote}</div> : <p>loading...</p>
+            }
+            <input placeholder="Enter author here" />
             <Link to="/">
                 <button>Click for home!</button>
             </Link>
