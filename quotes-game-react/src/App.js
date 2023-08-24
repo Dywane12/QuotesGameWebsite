5f0931a0751466fc9bde9342a870fbda8294eb44
Calin <acalin729@gmail.com>
2023-08-24 18:25:47 +0300

    quote works, all components are functional components

--- quotes-game-react/src/App.js
+++ quotes-game-react/src/App.js
@@ -1,35 +1,19 @@
+import React from 'react';
 import { Route, Routes } from 'react-router';
 import Home from './Home/Home';
 import Game from './Game/Game';
 import './App.css';
-import { Component } from 'react';
-import axios from 'axios';
 
-class App extends Component {
-
-  state = {
-    data: []
-  }
-
-  componentDidMount() {
-      axios.get('https://api.api-ninjas.com/v1/quotes?category=',{
-          headers: { 'X-Api-Key': 't5uH4CHnIvhCe1J1jemWVA==oKu96FgPeJT5o3np'}
-      }).then(resp => {
-          console.log(resp);
-          this.setState({data: resp.data});
-      })
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Routes>
-          <Route path='/' Component={Home} />
-          <Route path='game' element={<Game quote={this.state.data.quote} />} />
-        </Routes> 
-      </div>
+const App = () => {
+  
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' Component={Home} />
+        <Route path='game' element={<Game />} />
+      </Routes> 
+    </div>
     );
-  }
 }
 
 export default App;
--- quotes-game-react/src/Game/Game.js
+++ quotes-game-react/src/Game/Game.js
@@ -1,18 +1,30 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import axios from 'axios';
-
-const Game = (props) => {
+import axios from "axios";
 
 
+const Game = () => {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://api.api-ninjas.com/v1/quotes?category=',{
+            headers: { 'X-Api-Key': 't5uH4CHnIvhCe1J1jemWVA==oKu96FgPeJT5o3np'}
+        }).then(resp => {
+            console.log(resp);
+            setData(resp.data);
+        })
+    }, []) 
 
     return(
         <div className="components">
-            <h2>Who said that quote?</h2>
+            <h2>Who said this quote?</h2>
             {
-                props.quote ? <div>{props.quote}</div> : <p>loading...</p>
+                data[0] && data[0].quote ? <div>{data[0].quote}</div> : <p>loading...</p>
             }
-            <input placeholder="Enter author here" />
+            <form>
+                <input placeholder="Enter author here" />
+            </form>
             <Link to="/">
                 <button>Click for home!</button>
             </Link>
