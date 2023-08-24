706b9a973918a233b184487a0882b63de57b94a4
Calin <acalin729@gmail.com>
2023-08-24 20:55:43 +0300

    finished game, needs more styling + home page

--- quotes-game-react/src/Game/Game.js
+++ quotes-game-react/src/Game/Game.js
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import { Button, Input } from 'antd';
+import { LeftOutlined } from '@ant-design/icons';
 
 
 const Game = () => {
 
     const [data, setData] = useState([]);
+    const [guess, setGuess] = useState("");
+    const [correct, setCorrect] = useState(false);
+    const [wrong, setWrong] = useState(false);
+    const [points, setPoints] = useState(0);
+    const [win, setWin] = useState(false);
+
 
     useEffect(() => {
         axios.get('https://api.api-ninjas.com/v1/quotes?category=',{
@@ -16,19 +24,86 @@ const Game = () => {
         })
     }, []) 
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(guess === data[0].author) {
+                setCorrect(true);
+                setWrong(false);
+                setPoints(points + 1);
+                if(points == 5) {
+                    setWin(true);
+                }
+        }
+        else {
+            setCorrect(false);
+            setWrong(true);
+        }
+    }
+
+    const nextClickedHandler = () => {
+        axios.get('https://api.api-ninjas.com/v1/quotes?category=',{
+            headers: { 'X-Api-Key': 't5uH4CHnIvhCe1J1jemWVA==oKu96FgPeJT5o3np'}
+        }).then(resp => {
+            console.log(resp);
+            setData(resp.data);
+        });
+
+        setGuess("");
+        setCorrect(false);
+        setWrong(false);
+    }
+
     return(
-        <div className="components">
-            <h2>Who said this quote?</h2>
-            {
-                data[0] && data[0].quote ? <div>{data[0].quote}</div> : <p>loading...</p>
-            }
-            <form>
-                <input placeholder="Enter author here" />
-            </form>
-            <Link to="/">
-                <button>Click for home!</button>
-            </Link>
-        </div>
+        <>
+        <Link to="/">
+            <Button icon={<LeftOutlined />} style={{margin: 20}}>Back</Button>
+        </Link>
+        <div style={{ margin: 20, fontSize: 22, float: "right" }}>Points: {points}</div>
+        {
+            !win ? 
+            (
+                <div style={{textAlign: "center"}}> 
+                <h2 style={{margin: 25, fontSize: 25}}>Who said this quote?</h2>
+                {
+                    data[0] && data[0].quote ? <div style={{margin: 50, fontSize: 22}}>{data[0].quote}</div> : <p>loading...</p>
+                }
+                <form onSubmit={handleSubmit}>
+                        <Input 
+                            size="large"
+                            type="text" 
+                            value={guess} 
+                            onChange={(e) => {setGuess(e.target.value)}}
+                            placeholder="Author name" 
+                            style={{maxWidth: 250, margin: "auto", display: "block"}}
+                        />
+                        <Button 
+                        size="large"
+                        htmlType="submit"
+                        style={{margin: 10}}
+                        >Submit!</Button>
+                    </form>
+                { 
+                    correct ? (
+                                <div>
+                                    <p style={{margin: 10, fontSize: 22, color: "green"}}>Correct!</p>
+                                    <Button size="large" onClick={nextClickedHandler} >Next!</Button>
+                                </div>
+                            ) : null 
+                }
+                { 
+                    wrong ? (
+                                <div>
+                                    <p style={{margin: 10, fontSize: 22, color: "red"}}>Wrong!</p>
+                                    <Button size="large" onClick={nextClickedHandler} >Next!</Button>
+                                </div>
+                            ) : null 
+                }
+                
+                </div>
+            ) : <h1>You win!</h1>
+        }
+        
+        </>
     )
 }
 
